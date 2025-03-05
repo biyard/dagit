bc06aa0a11b3ad8557195079915d30684db1afd8
ryancho-0 <ryan@biyard.co>
2025-03-05 16:29:37 +0900

    fix(models): add actions to models

--- packages/models/src/lib.rs
+++ packages/models/src/lib.rs
@@ -1,46 +1,4 @@
-use validator::ValidationErrors;
-
+pub mod error;
 pub mod v1;
-pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(thiserror::Error, Clone, Debug, serde::Serialize, serde::Deserialize)]
-pub enum Error {
-    #[error("Unknown error: {0}")]
-    Unknown(String),
-    #[error("Validation error: {0}")]
-    ValidationError(String),
-    #[error("Service error: {0}")]
-    DatabaseError(String),
-    #[error("Reqwest error: {0}")]
-    ReqwestError(String),
-}
-
-impl From<reqwest::Error> for Error {
-    fn from(e: reqwest::Error) -> Self {
-        Self::ReqwestError(e.to_string())
-    }
-}
-
-impl From<ValidationErrors> for Error {
-    fn from(e: ValidationErrors) -> Self {
-        Self::ValidationError(e.to_string())
-    }
-}
-
-#[cfg(feature = "server")]
-impl From<sqlx::Error> for Error {
-    fn from(e: sqlx::Error) -> Self {
-        Error::DatabaseError(e.to_string())
-    }
-}
-
-#[cfg(feature = "server")]
-impl by_axum::axum::response::IntoResponse for Error {
-    fn into_response(self) -> by_axum::axum::response::Response {
-        (
-            by_axum::axum::http::StatusCode::BAD_REQUEST,
-            by_axum::axum::Json(self),
-        )
-            .into_response()
-    }
-}
+pub type Result<T> = std::result::Result<T, error::ServiceError>;
--- packages/models/src/v1/mod.rs
+++ packages/models/src/v1/mod.rs
@@ -2,3 +2,10 @@ pub mod agit;
 pub mod artist;
 pub mod artwork;
 pub mod collection;
+
+pub mod prelude {
+    pub use super::agit::*;
+    pub use super::artist::*;
+    pub use super::artwork::*;
+    pub use super::collection::*;
+}
