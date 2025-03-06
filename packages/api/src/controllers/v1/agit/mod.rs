e004ed7c9f4cc07c60ea141f4f76bc32f321e23f
JesusOfLagos <jesuswrites20043@gmail.com>
2025-03-06 15:37:37 +0100

    added the CRUD controllers for agit

--- packages/api/src/controllers/v1/agit/mod.rs
+++ packages/api/src/controllers/v1/agit/mod.rs
@@ -1,18 +1,20 @@
 #![allow(unused)]
 
 use by_axum::aide;
+use by_axum::axum::http::StatusCode;
 use by_axum::auth::Authorization;
 use by_axum::axum::extract::{Path, Query, State};
-use by_axum::axum::routing::post;
-use by_axum::axum::{Extension, Json};
+use by_axum::axum::routing::{get, post};
+use by_axum::axum::{Extension, Json, Router};
+use models::error::ServiceError;
 use models::Result;
 use models::v1::prelude::*;
+use sqlx::query;
+use std::sync::Arc;
 
 use super::collection::CollectionControllerV1;
 
-#[derive(
-    Debug, Clone, serde::Deserialize, serde::Serialize, schemars::JsonSchema, aide::OperationIo,
-)]
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize, schemars::JsonSchema, aide::OperationIo)]
 pub struct AgitPathParam {
     id: i64,
 }
@@ -29,47 +31,65 @@ impl AgitControllerV1 {
         Self { repo, pool }
     }
 
-    pub fn route(pool: sqlx::PgPool) -> Result<by_axum::axum::Router> {
+    pub fn route(pool: sqlx::PgPool) -> Result<Router> {
         let ctrl = Self::new(pool.clone());
-        Ok(by_axum::axum::Router::new()
-            .route("/:id", post(Self::act_by_id).get(Self::get))
-            .route("/", post(Self::act).get(Self::list))
+        Ok(Router::new()
+            .route("/:id", get(Self::get).post(Self::act_by_id))
+            .route("/", get(Self::list).post(Self::act))
             .with_state(ctrl)
             .nest(
                 "/:agit_id/collections",
                 CollectionControllerV1::route(pool.clone())?,
             ))
     }
-}
 
-impl AgitControllerV1 {
     pub async fn list(
         State(ctrl): State<AgitControllerV1>,
         Extension(claim): Extension<Option<Authorization>>,
         Query(q): Query<AgitParam>,
     ) -> Result<Json<Vec<AgitSummary>>> {
-        //TODO: Add Listing Agits
         tracing::debug!("list agits");
-        Ok(Json(vec![]))
+        let query = AgitQuery {
+            size: 10,
+            bookmark: None,
+        };
+
+        match ctrl.repo.find(&query).await {
+            Ok(agits) => Ok(Json(agits.items)), 
+            Err(e) => Err(ServiceError::Unknown(format!("Failed to retrieve agits: {}", e))),
+        }
     }
+
     pub async fn get(
         State(ctrl): State<AgitControllerV1>,
         Extension(claim): Extension<Option<Authorization>>,
         Path(AgitPathParam { id }): Path<AgitPathParam>,
     ) -> Result<Json<Agit>> {
-        tracing::debug!("get agit {id}");
-        Ok(Json(Agit::default()))
+        tracing::debug!("get agit by id {}", id);
+        let id_action = AgitReadAction { 
+            // TODO (rejects the id)
+            // id: id,
+         };
+
+        match ctrl.repo.find_one(&id_action).await {
+            Ok(agit) => Ok(Json(agit)),
+            Err(e) => Err(ServiceError::Unknown(format!("Failed to retrieve agit: {}", e).into())),
+        }
     }
+
     pub async fn act(
         State(ctrl): State<AgitControllerV1>,
         Extension(claim): Extension<Option<Authorization>>,
         Json(body): Json<AgitAction>,
     ) -> Result<Json<Agit>> {
         tracing::debug!("agit act {body:?}");
+        
         match body {
             AgitAction::Create(req) => {
-                //TODO: Add Create Agit
-                Ok(Json(Agit::default()))
+                match ctrl.repo.insert(req.title).await {
+                    Ok(agit) => Ok(Json(agit)),
+                    Err(e) => Err(ServiceError::Unknown(format!("Failed to create agit: {}", e).into())),
+                }
             }
         }
     }
@@ -81,14 +101,23 @@ impl AgitControllerV1 {
         Json(body): Json<AgitByIdAction>,
     ) -> Result<Json<Agit>> {
         tracing::debug!("agit act_by_id {id} {body:?}");
+
         match body {
-            AgitByIdAction::Update(_) => {
-                //TODO: Add Update Agit
-                Ok(Json(Agit::default()))
+            AgitByIdAction::Update(req) => {
+                let update_request = AgitRepositoryUpdateRequest {
+                    title: Some(req.title),
+                };
+
+                match ctrl.repo.update(id, update_request).await {
+                    Ok(updated_agit) => Ok(Json(updated_agit)),
+                    Err(e) => Err(ServiceError::Unknown(format!("Failed to update agit: {}", e).into())),
+                }
             }
             AgitByIdAction::Delete(_) => {
-                //TODO: Add Delete Agit
-                Ok(Json(Agit::default()))
+                match ctrl.repo.delete(id).await {
+                    Ok(_) => Ok(Json(Agit::default())), 
+                    Err(e) => Err(ServiceError::Unknown(format!("Failed to delete agit: {}", e).into())),
+                }
             }
         }
     }
